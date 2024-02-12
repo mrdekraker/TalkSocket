e7505c5d947dcf78d5b65c719a35609b016826bc
Mark DeKraker <mrdekraker@Marks-Laptop.local>
2024-02-12 03:41:27 -0600

    successfully completed friend add

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # TalkSocket
 
 NextJS & Socket.io simple Chat App.
+
+## Features
+
+- Real-time messaging
+- Google User Authentication
+- Responsive UI built with TailwindCSS
+- Redis for session storage
+- Conditional classes with clsx
+- Variants with class-variance-authority
--- talksocket/src/app/api/friends/add/route.ts
+++ talksocket/src/app/api/friends/add/route.ts
@@ -1,14 +1,62 @@
 import { addFriendValidator } from "@/lib/validations/add-friend";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { fetchRedis } from "@/helpers/redis";
+import { db } from "@/lib/db";
+import { z } from "zod";
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
 
     const { email: emailToAdd } = addFriendValidator.parse(body.email);
+
+    const idToAdd = await fetchRedis("get", `user:email:${emailToAdd}`);
+
+    if (!idToAdd) {
+      return new Response("User not found", { status: 400 });
+    }
+
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    if (idToAdd === session.user.id) {
+      return new Response("You cannot add yourself as a friend", {
+        status: 400,
+      });
+    }
+
+    const isAlreadyAdded = (await fetchRedis(
+      `sismember`,
+      `user:${idToAdd}:incoming_friend_requests`,
+      session.user.id
+    )) as 0 | 1;
+
+    if (isAlreadyAdded) {
+      return new Response("You already added this user", { status: 400 });
+    }
+
+    const isAlreadyFriends = (await fetchRedis(
+      `sismember`,
+      `user:${session.user.id}:friends`,
+      session.user.id
+    )) as 0 | 1;
+
+    if (isAlreadyFriends) {
+      return new Response("You are already friends", { status: 400 });
+    }
+
+    db.sadd(`user:${idToAdd}:incoming_friend_requests`, session.user.id);
+
+    return new Response("Friend request sent", { status: 200 });
   } catch (error) {
-    return {
-      status: 500,
-      body: { message: "An unknown error occurred" },
-    };
+    if (error instanceof z.ZodError) {
+      return new Response(`Invalid request payload`, { status: 422 });
+    }
+
+    return new Response("Invalid Request", { status: 400 });
   }
 }
