cead129cbe31196e2ff6399aebf4cc8ffbe62062
Mark DeKraker <mrdekraker@Marks-Laptop.local>
2024-02-12 08:37:17 -0600

    set up layout

---
+++ talksocket/src/app/(dashboard)/dashboard/layout.tsx
@@ -0,0 +1,78 @@
+import { getServerSession } from "next-auth";
+import { FC, ReactNode } from "react";
+import { authOptions } from "@/lib/auth";
+import { notFound } from "next/navigation";
+import Link from "next/link";
+import { Icon, Icons } from "@/components/Icons";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+interface SidebarOption {
+  id: number;
+  name: string;
+  href: string;
+  Icon: Icon;
+}
+
+const sidebarOptions: SidebarOption[] = [
+  {
+    id: 1,
+    name: "Add Friend",
+    href: "/dashboard/friends/add",
+    Icon: "UserPlus",
+  },
+];
+
+const Layout: FC<LayoutProps> = async ({ children }: LayoutProps) => {
+  const session = await getServerSession(authOptions);
+  if (!session) notFound();
+
+  return (
+    <div className="w-full flex h-screen">
+      <div className="flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
+        <Link href="/dashboard" className="flex h-16 shrink-0 items-center">
+          <Icons.Logo className="h-12 w-auto text-slate-600" />
+        </Link>
+
+        <div className="text-xs font-semibold leading-6 text-gray-400">
+          Your Chats
+        </div>
+
+        <nav className="flex flex-1 flex-col">
+          <ul role="list" className="flex flex-1 flex-col gap-y-7">
+            <li>// Chats this user has</li>
+            <li>
+              <div className="text-xs font-semibold leading-6 text-gray-400">
+                Overview
+              </div>
+
+              <ul role="list" className="mx-2 mt-2 space-y-1">
+                {sidebarOptions.map((option) => {
+                  const Icon = Icons[option.Icon];
+                  return (
+                    <li key={option.id}>
+                      <Link
+                        href={option.href}
+                        className="text-gray-700 hover:text-slate-600 hover:bg-gray-50 group flex gap-3 rounded-md p-2 text-sm leading-6 font-semibold">
+                        <span className="text-gray-400 border-gray-200 group-hover:border-slate-600 group-hover:text-slate-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white">
+                          <Icon className="h-4 w-4" />
+                        </span>
+
+                        <span className="truncate">{option.name}</span>
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
